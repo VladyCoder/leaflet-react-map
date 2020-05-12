231f53bc1865c058ba2a8441857d2fdfc904547c
VladyCoder <Illia_bilyk@outlook.com>
2020-05-12 23:36:09 +0800

    cluster marker

--- src/components/LeafletMap/index.js
+++ src/components/LeafletMap/index.js
@@ -1,5 +1,8 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/leaflet.markercluster';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 export var MapTypes = {
     _TRACKING_: 'TRACKING_MAP',
@@ -7,10 +10,17 @@ export var MapTypes = {
     _HEAT_: 'HEAT_MAP'
 }
 
+export var MapIcons = {
+    _RED_: 'assets/img/icons/r_marker.png',
+    _BULE_: 'assets/img/icons/b_marker.png',
+    _GREEN_: 'assets/img/icons/g_marker.png'
+}
+
 export class LMap {
     constructor(id, options, type = MapTypes._TRACKING_){
         this.map = L.map(id, options);
         this.mapType = type;
+        this.clusterGroup = L.markerClusterGroup();
     }
 
     setType(type = MapTypes._TRACKING_){
@@ -23,4 +33,28 @@ export class LMap {
     setTileLayer(templateUrl, options) {
         L.tileLayer(templateUrl, options).addTo(this.map);
     }
+
+    drawPath(latlngs, options){
+        if(!latlngs || !latlngs.length) return;
+        L.polyline(latlngs, options).addTo(this.map);
+    }
+
+    drawMarker(markers){
+        markers.forEach(m => {
+            var myIcon = L.icon({
+                iconUrl: m.icon,
+                iconSize: [32, 32],
+                iconAnchor: [16, 32],
+                popupAnchor: [-3, -76],
+                shadowUrl: null,
+                shadowSize: null,
+                shadowAnchor: null
+            });
+            var marker = L.marker(m.latlng, {icon: myIcon});
+            this.clusterGroup.addLayer(marker);
+        });
+
+        this.map.addLayer(this.clusterGroup);
+    }
+
 }
--- src/views/Maps/index.js
+++ src/views/Maps/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import { Row, Col, Card, CardBody, CardColumns, CardHeader } from 'reactstrap';
-import { LMap, MapTypes } from '../../components/LeafletMap';
+import { Row, Col, Card, CardBody, CardHeader } from 'reactstrap';
+import { LMap, MapTypes, MapIcons } from '../../components/LeafletMap';
 
 export default class Maps extends React.Component{
     componentDidMount(){
-        this.trackingMap = new LMap('mymap', { center: [58, 16], zoom: 6});
+        this.trackingMap = new LMap('tracking_map', { center: [50.073658, 14.418540], zoom: 6}, MapTypes._TRACKING_);
         this.trackingMap.setTileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             detectRetina: true,
             maxZoom: 19,
             maxNativeZoom: 17,
         });
 
-        this.trackingMap.setType(MapTypes._POSITION_);
+        this.trackingMap.drawPath([
+            [50.073658, 14.418540],
+            [51, 16],
+            [51.5, 15.8],
+            [52, 15],
+            [52, 13]
+        ], {color: 'red'});
+
+
+        this.positionMap = new LMap('position_map', { center: [50.073658, 14.418540], zoom: 6}, MapTypes._POSITION_);
+        this.positionMap.setTileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            detectRetina: true,
+            maxZoom: 19,
+            maxNativeZoom: 17,
+        });
+        this.positionMap.drawMarker([
+            {
+                latlng: [50.073658, 14.418540],
+                icon: MapIcons._RED_
+            },
+            {
+                latlng: [51, 16],
+                icon: MapIcons._BULE_
+            },
+            {
+                latlng: [51.5, 15.8],
+                icon: MapIcons._GREEN_
+            },
+            {
+                latlng: [52, 15],
+                icon: MapIcons._RED_
+            },
+            {
+                latlng: [52, 13],
+                icon: MapIcons._BULE_
+            }
+        ]);
     };
 
     render(){
@@ -24,10 +60,21 @@ export default class Maps extends React.Component{
                             Tracking Map
                         </CardHeader>
                         <CardBody>
-                            <div id="mymap" style={{height: '320px'}}></div>
+                            <div id="tracking_map" style={{height: '320px'}}></div>
                         </CardBody>
                     </Card>
                 </Col>
+                <Col sm="12" md="6">
+                    <Card>
+                        <CardHeader>
+                            Position Map
+                        </CardHeader>
+                        <CardBody>
+                            <div id="position_map" style={{height: '320px'}}></div>
+                        </CardBody>
+                    </Card>
+                </Col>
+                
             </Row>
         );
     }
